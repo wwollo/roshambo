a19e76612fbe7ce086ce4e80474149093c03109a
William Wan <wanwilliam@protonmail.com>
2021-02-12 01:03:30 -0500

    Implement Play 5 button functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const WIN = 'You win!';
 const LOSE = 'You lose!';
 const ERROR = 'Something went wrong.';
 const WRONG = 'Wrong game.'
+const INVALID = 'Invalid input. Try again.'
 
 // Listeners
 document.getElementById('game').onclick = function () {
@@ -28,7 +29,63 @@ document.getElementById('game').onclick = function () {
             output.innerHTML = `<p><a href="lizardspock/index.html">${WRONG}</a></p>`;
             break;
         default:
-            output.innerHTML = `<p>Invalid input. Try again.</p>`;
+            output.innerHTML = `<p>${INVALID}</p>`;
+    }
+}
+document.getElementById('game5').onclick = function () {
+    let scoreYou = 0;
+    let scoreCom = 0;
+    let exit = '';
+    let output = document.getElementById('output');
+    output.innerHTML = '';
+
+    for (i = 0; i<5; i++) {
+        let result = game();
+
+        switch (result[2]) {
+            case TIE:
+                break;
+            case WIN:
+                scoreYou++;
+                break;
+            case LOSE:
+                scoreCom++;
+                break;
+            case ERROR:
+            case WRONG:
+                exit = result[2];
+                i = 5;
+                break;
+            default:
+                exit = INVALID
+                i = 5;
+        }
+    }
+
+    switch (true) {
+        case Boolean(exit):
+            switch (true) {
+                case (exit == ERROR):
+                    output.innerHTML = `<p>${ERROR}</p>`;
+                    break;
+                case (exit == WRONG):
+                    output.innerHTML = `<p><a href="lizardspock/index.html">${WRONG}</a></p>`;
+                    break;
+                default:
+                    output.innerHTML = `<p>${INVALID}</p>`;
+            }
+            break;
+        default:
+            switch (true) {
+                case (scoreYou > scoreCom):
+                    output.innerHTML = `<p>You won ${scoreYou} points to ${scoreCom} points. You won ${scoreYou} times, lost ${scoreCom} times, and tied ${5-scoreYou-scoreCom} times.</p>`;
+                    break;
+                case (scoreYou < scoreCom):
+                    output.innerHTML = `<p>You lost ${scoreYou} points to ${scoreCom} points. You won ${scoreYou} times, lost ${scoreCom} times, and tied ${5-scoreYou-scoreCom} times.</p>`;
+                    break;
+                default:
+                    output.innerHTML = `<p>You tied ${scoreYou} points to ${scoreCom} points. You won ${scoreYou} times, lost ${scoreCom} times, and tied ${5-scoreYou-scoreCom} times.</p>`;
+            }
     }
 }
 
@@ -97,7 +154,7 @@ function game() {
                     result[2] = WIN;
                     break;
                 case 'Scissors':
-                    result[2] = TIE;
+                    result[2] = TIE;leotard
                     break;
                 default:
                     result[2] = ERROR;
@@ -110,7 +167,7 @@ function game() {
             return result;
             break;
         default:
-            result[2] = 'Invalid input. Try again.';
+            result[2] = INVALID;
             return result;
     }
 }
